9ea99f61419b025da7f57a1f1546fa51bd016b59
Tobias Dühr <nerd@tobiasduehr.com>
2017-09-20 22:56:39 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# remote-sync-ds
-Standalone script, mimicking the function of the Atom package "remote-sync"
+# Remote Sync DS
+
+Standalone script, mimicking the function of the Atom package "remote-sync".
+
+## Installation
+
+Add to project, where you are using "remote-sync" (where .remote-sync.json is located)
+
+`yarn add remote-sync-ds`
+
+Add to your package.json in `scripts`
+
+	"scripts": {
+		…
+    	"sync": "remote-sync-ds",
+    	…
+	}
+
+## Usage
+
+Enter `npm run sync`. Every file changed will be uploaded to the server defined in `.remote-sync`.
---
+++ remote-sync-ds.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const chokidar = require('chokidar');
+const config = require('./.remote-sync.json');
+const minimatch = require('minimatch');
+const EasyFtp = require('easy-ftp');
+const ftp = new EasyFtp();
+let queue = [];
+let timeout;
+
+const watcher = chokidar.watch(process.cwd(), {
+	ignored: config.ignore,
+	ignoreInitial: true,
+	followSymlinks: false,
+	persistent: true,
+	atomic: true
+});
+
+ftp.connect({
+	type: config.transport,
+	port: config.port,
+    host: config.hostname,
+	user: config.username,
+	password: config.password
+});
+
+function workQueue(){
+	const files = [];
+	for(let i = 0; i < queue.length; i++){
+		files.push({
+			local: queue[i],
+			remote: path.resolve(config.target, queue[i])
+		});
+		console.log(queue[i], ' => ', path.resolve(config.target, queue[i]));
+	}
+	queue = [];
+	ftp.upload(files);
+}
+
+function addFile(file){
+	const _p = path.relative(process.cwd(), file);
+	for(let i = 0; i < config.ignore.length; i++){
+		if(minimatch(_p, config.ignore[i], { matchBase: true, dot: true })) return;
+	}
+	if(queue.indexOf(_p) < 0) queue.push(_p);
+	clearTimeout(timeout);
+	timeout = setTimeout(workQueue, 1000);
+}
+
+watcher
+	.on('add', addFile)
+	.on('change', addFile)
+	.on('unlink', path => console.log(`File ${path} has been removed`));
