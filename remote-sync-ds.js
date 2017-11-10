a2ac3d42cd28e185ac0f5b9a46243708c0be1afe
Tobias Duehr <tobias@dieschittigs.de>
2017-11-10 11:59:51 +0100

    better output, notifications

--- remote-sync-ds.js
+++ remote-sync-ds.js
@@ -8,22 +8,24 @@ const minimatch = require('minimatch');
 const EasyFtp = require('easy-ftp');
 const _ = require('lodash');
 const chalk = require('chalk');
+const moment = require('moment');
+const notifier = require('node-notifier');
 const log = console.log;
 
 let config;
-try{
+try {
     config = require(path.resolve(process.cwd(), '.remote-sync.json'));
-} catch (e){
+} catch (e) {
     console.log('No ".remote-sync" found!');
     process.exit(1);
 }
 
 let ignore = [];
-if(config.ignore) ignore = ignore.concat(config.ignore);
+if (config.ignore) ignore = ignore.concat(config.ignore);
 
-if(config.watch){
+if (config.watch) {
     _.forEach(config.watch, (pattern) => {
-        if(pattern.substr(0,1) == '/') config.watch.push(pattern.substr(1));
+        if (pattern.substr(0, 1) == '/') config.watch.push(pattern.substr(1));
     });
 }
 
@@ -34,22 +36,22 @@ const ftp = new EasyFtp();
 
 let watcher;
 
-try{
+try {
     ftp.connect({
         type: config.transport || 'ftp',
-        port: config.port  || 21,
+        port: config.port || 21,
         host: config.hostname,
         user: config.username,
         password: config.password
     });
-} catch (e){
+} catch (e) {
     log(chalk.error(e));
 }
 
-function _setupWatcher(filesToWatch = null){
-    if(watcher) watcher.close();
-    if(!filesToWatch) filesToWatch = process.cwd();
-    else if(config.watch){
+function _setupWatcher(filesToWatch = null) {
+    if (watcher) watcher.close();
+    if (!filesToWatch) filesToWatch = process.cwd();
+    else if (config.watch) {
         filesToWatch = filesToWatch.concat(config.watch);
     }
     watcher = chokidar.watch(filesToWatch, {
@@ -64,47 +66,74 @@ function _setupWatcher(filesToWatch = null){
     //.on('unlink', path => console.log(`File ${path} has been removed`));
 }
 
-function setupWatcher(){
+function setupWatcher(initial = false) {
     exec("git ls-files", function(err, stdout, stderr) {
-        if(!stderr) _setupWatcher(stdout.split('\n'));
-        else _setupWatcher();
+        if (!stderr) {
+            const filesFromGit = _.compact(stdout.split('\n'));
+            _setupWatcher(filesFromGit);
+            if (!initial) return;
+            filesFromGit.forEach((file) => {
+                log(
+                    chalk.white(moment().format('HH:mm:ss')),
+                    chalk.green('Watching'),
+                    chalk.blue(file)
+                );
+            });
+            notifier.notify({
+                'title': 'Remote Sync DS',
+                'message': 'Ready, watching ' + filesFromGit.length + ' files'
+            });
+        } else {
+            _setupWatcher();
+            if (!initial) return;
+            log(
+                chalk.white(moment().format('HH:mm:ss')),
+                chalk.green('Watching local directory')
+            );
+        }
     });
 }
 
-function workQueue(){
+function workQueue() {
     const files = [];
-    _.forEach(queue, (entry)=>{
+    _.forEach(queue, (entry) => {
         const remote = path.resolve(config.target, entry);
         files.push({
             local: entry,
             remote: remote
         });
         log(
+            chalk.white(moment().format('HH:mm:ss')),
+            chalk.green('Uploading'),
             chalk.blue(entry),
             '~>',
             chalk.green(remote)
         );
     });
     queue = [];
-    try{
+    try {
         ftp.upload(files);
-    } catch (e){
+        notifier.notify({
+            'title': 'Remote Sync DS',
+            'message': 'Uploaded ' + files.length + ' files'
+        });
+    } catch (e) {
         console.log(chalk.error(e));
     }
     setupWatcher();
 }
 
-function isValidFile(file){
+function isValidFile(file) {
     isValid = true;
-    _.forEach(ignore, (pattern)=>{
-        if(minimatch(file, pattern, { matchBase: true, dot: true })){
+    _.forEach(ignore, (pattern) => {
+        if (minimatch(file, pattern, { matchBase: true, dot: true })) {
             isValid = false;
             return;
         }
     });
-    if(!isValid && config.watch){
-        _.forEach(config.watch, (pattern)=>{
-            if(minimatch(file, pattern, { matchBase: true, dot: true })){
+    if (!isValid && config.watch) {
+        _.forEach(config.watch, (pattern) => {
+            if (minimatch(file, pattern, { matchBase: true, dot: true })) {
                 isValid = true;
                 return;
             }
@@ -113,15 +142,19 @@ function isValidFile(file){
     return isValid;
 }
 
-function addFile(file){
+function addFile(file) {
     const _p = path.relative(process.cwd(), file);
-    if(!isValidFile(_p)){
-        log(chalk.yellow('Ignoring ' + _p));
+    if (!isValidFile(_p)) {
+        log(
+            chalk.white(moment().format('HH:mm:ss')),
+            chalk.yellow('Ignoring'),
+            chalk.blue(_p)
+        );
         return;
     }
-    if(queue.indexOf(_p) < 0) queue.push(_p);
+    if (queue.indexOf(_p) < 0) queue.push(_p);
     clearTimeout(timeout);
     timeout = setTimeout(workQueue, 1000);
 }
 
-setupWatcher();
+setupWatcher(true);
